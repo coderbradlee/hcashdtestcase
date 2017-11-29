820b94e5d7fed74e543955f264fb5a0c5c4b3956
limingming <limingming@wident.cn>
2017-11-29 14:41:43 +0800

    init

--- test.go
+++ test.go
@@ -60,6 +60,36 @@ func test1() {
 	log.Printf("Size (in bytes): %v\n", block.Size)
 	log.Printf("Num transactions: %v\n", len(block.Tx))
 }
+func test2() {
+	hcashdHomeDir := hcashutil.AppDataDir(".hcashd", false)
+	certs, err := ioutil.ReadFile(filepath.Join(hcashdHomeDir, "rpc.cert"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new RPC client using websockets.  Since this example is
+	// not long-lived, the connection will be closed as soon as the program
+	// exits.
+	connCfg := &hcashrpcclient.ConnConfig{
+		Host:         "localhost:12010",
+		Endpoint:     "ws",
+		User:         "bitcoinrpc",
+		Pass:         "123456",
+		Certificates: certs,
+	}
+	client, err := hcashrpcclient.New(connCfg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Shutdown()
+
+	// Query the RPC server for the current block count and display it.
+	blockCount, err := client.GetBlockCount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Block count: %d", blockCount)
+}
 func main() {
-	test1()
+	test2()
 }
