7ed5262f6d9cf695f45f5f3dff5bc559ce85d897
limingming <limingming@wident.cn>
2017-11-29 17:05:26 +0800

    init

--- wallet/config.go
+++ wallet/config.go
@@ -30,7 +30,7 @@ const (
 
 var (
 	hcashdHomeDir            = hcashutil.AppDataDir(".hcashd", false)
-	hcashctlHomeDir          = hcashutil.AppDataDir("hcashdtestcase/wallet", false)
+	hcashctlHomeDir          = hcashutil.AppDataDir("/hcashdtestcase/wallet", false)
 	hcashwalletHomeDir       = hcashutil.AppDataDir(".hcashwallet", false)
 	defaultConfigFile      = filepath.Join(hcashctlHomeDir, "hcashctl.conf")
 	defaultRPCServer       = "localhost"
