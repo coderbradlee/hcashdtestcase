f429e775eab4e2d4efc7828861402b8ff3bcb811
limingming <limingming@wident.cn>
2017-11-30 09:58:04 +0800

    init

--- test.go
+++ test.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"path/filepath"
 	"time"
+	"bufio"
 	"github.com/HcashOrg/hcashd/chaincfg/chainhash"
+	"github.com/HcashOrg/hcashwallet/loader"
 )
 func test1() {
 	// Load the certificate for the TLS connection which is automatically
@@ -89,6 +91,62 @@ func test2() {
 		log.Fatal(err)
 	}
 	log.Printf("Block count: %d", blockCount)
+}
+// networkDir returns the directory name of a network directory to hold wallet
+// files.
+func networkDir(dataDir string, chainParams *chaincfg.Params) string {
+	netname := chainParams.Name
+
+	// For now, we must always name the testnet data directory as "testnet"
+	// and not "testnet" or any other version, as the chaincfg testnet
+	// paramaters will likely be switched to being named "testnet" in the
+	// future.  This is done to future proof that change, and an upgrade
+	// plan to move the testnet data directory can be worked out later.
+	switch chainParams.Net {
+	case wire.TestNet2:
+		netname = "testnet2"
+	}
+
+	return filepath.Join(dataDir, netname)
+}
+// createWallet prompts the user for information needed to generate a new wallet
+// and generates the wallet accordingly.  The new wallet will reside at the
+// provided path. The bool passed back gives whether or not the wallet was
+// restored from seed, while the []byte passed is the private password required
+// to do the initial sync.
+func createWallet(cfg *config) error {
+	dbDir := networkDir(cfg.AppDataDir.Value, activeNet.Params)
+	stakeOptions := &loader.StakeOptions{
+		VotingEnabled: cfg.EnableVoting,
+		AddressReuse:  cfg.ReuseAddresses,
+		TicketAddress: cfg.TicketAddress,
+		TicketFee:     cfg.TicketFee.ToCoin(),
+	}
+	loader := loader.NewLoader(activeNet.Params, dbDir, stakeOptions,
+		cfg.AddrIdxScanLen, cfg.AllowHighFees, cfg.RelayFee.ToCoin())
+
+	reader := bufio.NewReader(os.Stdin)
+	privPass, pubPass, seed, err := prompt.Setup(reader,
+		[]byte(wallet.InsecurePubPassphrase), []byte(cfg.createPass), []byte(cfg.WalletPass))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Creating the wallet...")
+	_, err = loader.CreateNewWallet(pubPass, privPass, seed)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("The wallet has been created successfully.")
+
+	return nil
+}
+func testcreatewallet() {
+	tcfg, _, err := loadConfig()
+	if err != nil {
+		fmt.Println(err) 
+	}
 }
 func main() {
 	test2()
